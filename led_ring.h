7530f53edbefa76633fa473bde839a0c521a6c68
Gene Cheng <gnencheng@exosite.com>
2015-05-14 09:49:32 +0800

    removed unnecessary files

--- led_ring.h
+++
@@ -1,8 +0,0 @@
-
-//*****************************************************************************
-//
-// Prototypes for the APIs.
-//
-//*****************************************************************************
-extern void LedRingLine(void);
-extern void Send24LedData(unsigned char Percent);
